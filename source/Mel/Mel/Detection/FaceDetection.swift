6428470498f5bcffba1ad62b493f2cc4ff4ce676
Patrik Tomas Chamelo <nscoding@fb.com>
2020-10-26 21:20:11 -0700

    [mel] Add documentation for face detection

--- source/Mel/Mel/Detection/FaceDetection.swift
+++ source/Mel/Mel/Detection/FaceDetection.swift
@@ -12,16 +12,19 @@ import SwiftUI
 
 enum FaceDetection {
   
+  // Returns the rectangles on an image for all the faces detected
   static func rects(for image: UIImage) -> [CGRect] {
     guard let ciImage = CIImage(image: image) else {
       return []
     }
-
-    let faceDetector = CIDetector(ofType: CIDetectorTypeFace,
-                                 context: nil,
-                                 options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])!
+    
+    let faceDetector =
+      CIDetector(ofType: CIDetectorTypeFace,
+                 context: nil,
+                 options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])!
     let faces = faceDetector.features(in: ciImage)
     return faces.map { $0.bounds }
   }
+  
 }
 
