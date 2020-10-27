7d3f7b23c2c60834a1726da2b10f5bfbd3b3ed6b
Patrik Tomas Chamelo <nscoding@fb.com>
2020-10-26 18:25:02 -0700

    [mel] Adding the basic app structure

---
+++ source/Mel/Mel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mel
+//
+//  Created by Patrik Tomas Chamelo on 10/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ source/Mel/Mel/MelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelApp.swift
+//  Mel
+//
+//  Created by Patrik Tomas Chamelo on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct MelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
